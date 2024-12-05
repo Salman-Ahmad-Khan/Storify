a387e669238178c7e3ed4b85170131a06ba4f596
Salman-Ahmad-Khan <connect.celman@gmail.com>
2024-12-05 21:21:04 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen text-brand font-bold items-center justify-center text-3xl">
+      <h1>Storify - The only storage solution you need.</h1>
+    </div>
+  );
+}
